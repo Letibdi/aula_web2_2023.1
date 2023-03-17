07ddcdab79cdea1e0a5de3bee9d22ed173b58eb1
Letibdi <114108449+Letibdi@users.noreply.github.com>
2023-03-17 15:16:50 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <a href="./View/UsuarioForm.php">Cadastrar </a><br>
+    <a href="./View/UsuarioList.php">Listar </a>
+  </body>
+</html>
